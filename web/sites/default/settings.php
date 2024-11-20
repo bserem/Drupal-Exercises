b25dcd88413bca343a0cb9c63b67824033c3a398
Ralphas <rafasmour@gmail.com>
2024-11-20 07:40:30 +0200

    exported config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -288,7 +288,7 @@ $databases = [];
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'luWwFfSU2OdV2hxlk1GfkR0R24kQ3LpKbs_jLrSLECrxM8GPC343tHqb9koU_Mh35a52wIQz1A';
 
 /**
  * Deployment identifier.
@@ -897,3 +897,15 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+$databases['default']['default'] = array (
+  'database' => 'db',
+  'username' => 'db',
+  'password' => 'db',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => 3306,
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
