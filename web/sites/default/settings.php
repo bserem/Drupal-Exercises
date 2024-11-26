b9c0e824ac3a50ab670ba33591fccb5751f5a2c3
Oikon <oikonomoum75@gmail.com>
2024-11-26 21:50:03 +0200

    Σετάρισμα του φακέλου config/sync και ρύθμιση στο settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -268,7 +268,8 @@ $databases['default']['default'] = [
  * directory in the public files path. The setting below allows you to set
  * its location.
  */
-# $settings['config_sync_directory'] = '/directory/outside/webroot';
+ $settings['config_sync_directory'] = '../config/sync';
+
 
 /**
  * Settings:
