ec1420b0aaa451cbdd1f496ba69ac8af1f289fa8
Oikon <oikonomoum75@gmail.com>
2024-12-05 17:10:04 +0200

    Stark subtheme

---
+++ web/themes/custom/stark_subtheme/js/script.js
@@ -0,0 +1,8 @@
+(function ($, Drupal) {
+    Drupal.behaviors.starkSubthemeBehavior = {
+      attach: function (context, settings) {
+        console.log('Subtheme Js loaded');
+      }
+    };
+  })(jQuery, Drupal);
+  
